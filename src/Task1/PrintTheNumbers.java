456d036c83e437bb33d6537a01f2d8a6d77d76b3
Sri Akshaya <akshayasenthil16@gmail.com>
2025-06-22 13:31:27 +0530

    Update PrintTheNumbers.java

--- src/Task1/PrintTheNumbers.java
+++ src/Task1/PrintTheNumbers.java
@@ -1,5 +1,17 @@
 package Task1;
-
+import java.util.Scanner;
 public class PrintTheNumbers {
+  public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		System.out.println("Enter the starting number");
+		int m =  inputScanner.nextInt( );
+		System.out.println("Enter the ending number");
+		int n  =  inputScanner.nextInt();
+		for(int i=m; i<=n; i ++) 
+		{
+			System.out.println(i);
+		}
+		inputScanner.close();
+	}
 
 }

f44ba293ede6192cc5e811d583afb9bc26ee92fd
Sri Akshaya <akshayasenthil16@gmail.com>
2025-06-22 13:32:52 +0530

    Update SmallestNumber.java

--- src/Task1/SmallestNumber.java
+++ src/Task1/SmallestNumber.java
@@ -1,5 +1,32 @@
 package Task1;
-
+import java.util.Scanner;
 public class SmallestNumber {
+  public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = inputScanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = inputScanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = inputScanner.nextInt();
+
+        int smallest = a;
+
+        if (b < smallest) {
+            smallest = b;
+        }
+        if (c < smallest) {
+            smallest = c;
+        }
+
+        System.out.println("The smallest number is: " + smallest);
+
+        inputScanner.close();
+	
+
+	}
 
 }

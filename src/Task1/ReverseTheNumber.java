cbf5dc8b98d3732da87ebb55f87f470cd37f59f4
Sri Akshaya <akshayasenthil16@gmail.com>
2025-06-22 13:32:07 +0530

    Update ReverseTheNumber.java

--- src/Task1/ReverseTheNumber.java
+++ src/Task1/ReverseTheNumber.java
@@ -1,5 +1,21 @@
 package Task1;
-
+import java.util.Scanner;
 public class ReverseTheNumber {
+  public static void main(String[] args) {
+	Scanner	inputScanner = new Scanner(System.in);
+	System.out.println("Enter the Number");
+	int number = inputScanner.nextInt();
+	int reversed = 0;
+
+    while (number != 0) {
+        int digit = number % 10;
+        reversed = reversed * 10 + digit;
+        number /= 10;
+    }
+
+    System.out.println("Reversed number : " + reversed);
+    inputScanner.close();
+}
+
 
 }

60fa6c6d241b4248b7efb3ef4d2345ed9ba11993
Sri Akshaya <akshayasenthil16@gmail.com>
2025-06-22 13:30:43 +0530

    Update NumberPattern.java

--- src/Task1/NumberPattern.java
+++ src/Task1/NumberPattern.java
@@ -1,5 +1,27 @@
 package Task1;
-
+import java.util.Scanner;
 public class NumberPattern {
+  public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = inputScanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > n - i - 1; j--) {
+                System.out.print(j);
+            }
+            for (int k = 0; k < n - i - 1; k++) {
+                System.out.print(n - i);
+            }
+            System.out.println();
+        }
+
+        // Final line (e.g., 54321)
+        for (int j = n; j >= 1; j--) {
+            System.out.print(j);
+        }
+
+        inputScanner.close();
+	}
 
 }

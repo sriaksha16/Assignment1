5f7b2c0c06f6b0b3fb1c07f66da679be9ba4d859
Sri Akshaya <akshayasenthil16@gmail.com>
2025-06-22 13:28:33 +0530

    Update PositiveNegative.java

--- src/Task1/PositiveNegative.java
+++ src/Task1/PositiveNegative.java
@@ -1,5 +1,20 @@
 package Task1;
-
+import java.util.Scanner;
 public class PositiveNegative {
+  public static void main(String[] args) {
+		Scanner inputScannner = new Scanner(System.in);
+		System.out.print("Enter the number ");
+		int n = inputScannner.nextInt();
+		
+		if( n<0) {
+				System.out.println("Negative" );				
+		}
+		else 
+		{
+			System.out.println("Positive");
+		}
+		inputScannner.close();
+		
+	}
 
 }
